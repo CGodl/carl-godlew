bb9b4f80775130cf02e120877db2966caa40449d
Carl Godlewski <cgodlew@gmail.com>
2024-04-27 18:59:46 -0400

    Adjusted stylng NavBar

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -9,13 +9,13 @@ export const NavBar = () => {
 	];
 
 	return (
-		<nav className='bg-slate-300 px-4 py-2 rounded-3xl backdrop-blur-sm w-fit'>
+		<nav className='bg-slate-300 px-4 py-2 rounded-3xl backdrop-blur-sm w-fit bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60'>
 			<ul className='flex items-center justify-center gap-4'>
 				{navigation.map((item) => (
 					<Link
 						key={item.href}
 						href={item.href}
-						className='text-sm duration-500 text-zinc-500'
+						className='text-sm duration-500 text-zinc-600'
 					>
 						{item.name}
 					</Link>
