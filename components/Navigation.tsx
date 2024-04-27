7d9f1f135351a062b2984ecc311438be9b87b9b2
Carl Godlewski <cgodlew@gmail.com>
2024-04-27 19:12:00 -0400

    Fixed NavBar to top-right of page

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -6,16 +6,17 @@ export const NavBar = () => {
 		{ name: 'Bio', href: '/' },
 		{ name: 'Resume', href: '/' },
 		{ name: 'Contact', href: '/contact' },
+		
 	];
 
 	return (
-		<nav className='bg-slate-300 px-4 py-2 rounded-3xl backdrop-blur-sm w-fit bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60'>
+		<nav className='fixed top-10 right-10 bg-slate-300 px-4 py-2 rounded-3xl backdrop-blur-sm w-fit bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60'>
 			<ul className='flex items-center justify-center gap-4'>
 				{navigation.map((item) => (
 					<Link
 						key={item.href}
 						href={item.href}
-						className='text-sm duration-500 text-zinc-600'
+						className='text-sm duration-500 text-zinc-700'
 					>
 						{item.name}
 					</Link>
