7310c1e63801b0246897d7ad6d64e76ab07f8fb8
Carl Godlewski <cgodlew@gmail.com>
2026-01-23 12:14:59 -0500

    Work so far

---
+++ components/HeaderImage.tsx
@@ -0,0 +1,11 @@
+
+
+export const SplashImage = () => {
+    return (
+        <section className="flex flex-col items-center justify-center w-full h-screen bg-[url('/spaceheader.png')] bg-cover bg-no-repeat">
+            <div className="w-full h-screen bg-blackOverlay">
+                <h1 className="font-knewave text-4xl text-white">Prepared for Space and Beyond!</h1>
+            </div>
+        </section>
+    )
+}
